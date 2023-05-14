c1ae7f912c50b778540c749b585f0cd595492a3f
violatv <130522162+violatv@users.noreply.github.com>
2023-05-14 05:12:52 +0300

    Update and rename Windows10ColabNoVT-X.sh to Windows10Colab.sh

--- Windows10Colab.sh
+++ Windows10Colab.sh
@@ -1,12 +1,11 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
-./ngrok authtoken $CRP 
+./ngrok authtoken 2FHQ9SC0PteACNxKnJLZlGSLo4o_83fgAadbdrp8sWhLrVWzv 
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
-sudo apt update -y > /dev/null 2>&1
+apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
-sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
+apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
 curl -L -o lite10.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.1.3/providers/qemu.box
 echo "Windows 10 x64 Lite On Google Colab"
@@ -15,6 +14,6 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by fb.com/thuong.hai.581
-sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=8  -m 10000M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=96  -m 10000M -machine usb=on -device usb-tablet > /dev/null 2>&1
 
 
