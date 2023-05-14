811fbb9f8f62c9c2407a72e8f6f84e8b27273819
violatv <130522162+violatv@users.noreply.github.com>
2023-05-14 05:28:18 +0300

    Update Win7VNC.sh

--- Win7VNC.sh
+++ Win7VNC.sh
@@ -1,7 +1,9 @@
+apt update
+apt install screen -y
+apt install sudo -y
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
-./ngrok authtoken $CRP 
+./ngrok authtoken 2FHQ9SC0PteACNxKnJLZlGSLo4o_83fgAadbdrp8sWhLrVWzv 
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
 sudo apt update -y > /dev/null 2>&1
