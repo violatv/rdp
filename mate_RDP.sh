c442bbe28b10765fb68ee6ee689b81d58316fa4a
violatv <130522162+violatv@users.noreply.github.com>
2023-05-14 04:18:07 +0300

    Update mate_RDP.sh

--- mate_RDP.sh
+++ mate_RDP.sh
@@ -9,7 +9,6 @@ apt update && apt upgrade -y
 apt install firefox -y
 apt-get install -y xrdp
 apt-get install mate-core mate-desktop-environment mate-notification-daemon -y
-sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
 service xrdp start
 clear
 echo XRDP Address:
