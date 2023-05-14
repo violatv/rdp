d1587db7379d06406f17a863a144906ad23bdb47
violatv <130522162+violatv@users.noreply.github.com>
2023-05-14 04:14:41 +0300

    Update mate_RDP.sh

--- mate_RDP.sh
+++ mate_RDP.sh
@@ -5,12 +5,12 @@ unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 ./ngrok authtoken 2FHQ9SC0PteACNxKnJLZlGSLo4o_83fgAadbdrp8sWhLrVWzv
 nohup ./ngrok tcp 3389 &>/dev/null &
 echo Downloading File From akuh.net
-sudo apt update && apt upgrade -y
-sudo apt install firefox -y
-sudo apt-get install -y xrdp
-sudo apt-get install mate-core mate-desktop-environment mate-notification-daemon -y
-sudo sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
-sudo service xrdp start
+apt update && apt upgrade -y
+apt install firefox -y
+apt-get install -y xrdp
+apt-get install mate-core mate-desktop-environment mate-notification-daemon -y
+sed -i.bak '/fi/a #xrdp multiple users configuration \n mate-session \n' /etc/xrdp/startwm.sh
+service xrdp start
 clear
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
