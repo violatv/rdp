7faed7ce72522c1fa10148fc5a4d04ad8deced61
violatv <130522162+violatv@users.noreply.github.com>
2023-05-21 03:23:48 +0300

    Update lxde_rdp.sh

--- lxde_rdp.sh
+++ lxde_rdp.sh
@@ -2,42 +2,30 @@ rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
 wget -O ng.sh https://raw.githubusercontent.com/violatv/rdp/main/GCngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
-clear
-./ngrok tcp --region us 3389 &>/dev/null &
-clear
 echo "================================"
 echo "Wait Downloading file 3-4 minutes"
 echo "More free vps visit 👉akuh.net👈"
 echo "================================"
-apt-get install lxde -y
-clear
-echo "===================================="
-echo "Install Firefox"
-echo "===================================="
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A6DCF7707EBC211F
-apt-add-repository "deb http://ppa.launchpad.net/ubuntu-mozilla-security/ppa/ubuntu bionic main"
-echo "===================================="
-echo "Update"
-echo "===================================="
-apt-get update > /dev/null 2>&1
-echo "===================================="
-echo "Installing"
-echo "===================================="
-apt-get install firefox -y > /dev/null 2>&1
-echo "===================================="
-echo "Install xrdp"
-echo "===================================="
-apt install -y xrdp > /dev/null 2>&1
-service xrdp start > /dev/null 2>&1
-clear
+sudo apt-get install lxde > /dev/null 2>&1
+echo "===========40%"
+sudo apt install -y xrdp > /dev/null 2>&1
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
+echo "===============60%"
+echo "==============="
+echo "Install Brave"
+echo "==============="
+sudo apt install apt-transport-https curl > /dev/null 2>&1
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt install brave-browser > /dev/null 2>&1
+echo "===================80%"
+sudo service xrdp start > /dev/null 2>&1
 echo "=======================100%"
-echo "===================================="
-echo "Username : root"
-echo "Username : that you made yourself"
-echo "RDP Address:"
+echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
-echo "Don't close this tab to keep RDP running"
+echo "Don't close tab colab to keep rdp running 12 hours"
 echo "👉Keep support akuh.net thank you👈"
 echo "===================================="
 sleep 9876543210
