e2060f9ae9fa6e36be6d3988e1a3cc40ef662fa0
violatv <130522162+violatv@users.noreply.github.com>
2023-05-23 01:55:59 +0300

    Update xfce4.sh

--- new_rdp/xfce4.sh
+++ new_rdp/xfce4.sh
@@ -1,27 +1,31 @@
 echo "Download ngrok"
-wget https://raw.githubusercontent.com/akuhnet/w-colab/main/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
+wget https://github.com/violatv/rdp/raw/main/new_rdp/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
 echo Downloading File From akuh.net
 echo "===================================="
 echo "Wait 2 Minutes"
 echo "This is only for education"
 echo "Other interesting tutorials visit akuh.net"
 echo "===================================="
-sudo apt-get update > /dev/null 2>&1
+sudo apt-get update
 echo "===================================="
-echo "Install Firefox"
-echo "===================================="
-sudo apt install firefox -y > /dev/null 2>&1
+echo "Install Brave"
+echo "==============="
+sudo apt install apt-transport-https curl > /dev/null 2>&1
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg > /dev/null 2>&1
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list > /dev/null 2>&1
+sudo apt update > /dev/null 2>&1
+sudo apt install brave-browser > /dev/null 2>&1
 echo "===================================="
 echo "Install RDP"
 echo "===================================="
-sudo apt install -y xrdp > /dev/null 2>&1
-sudo apt install xfce4 -y > /dev/null 2>&1
-sudo apt-get install xfce4 xfce4-terminal -y > /dev/null 2>&1
+sudo apt install xrdp -y
+sudo apt install xfce4 -y
+sudo apt-get install xfce4 xfce4-terminal -y
 echo "===================================="
 echo "Start RDP"
 echo "===================================="
-sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh > /dev/null 2>&1
-sudo service xrdp start > /dev/null 2>&1
+sudo sed -i.bak '/fi/a xfce4-session \n' /etc/xrdp/startwm.sh
+sudo service xrdp start
 echo XRDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "===================================="
