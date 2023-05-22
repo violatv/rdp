72853fe4c832e6420960f64bd00188f637dd14fa
violatv <130522162+violatv@users.noreply.github.com>
2023-05-23 02:11:33 +0300

    Update ngrok.sh

--- new_rdp/ngrok.sh
+++ new_rdp/ngrok.sh
@@ -2,9 +2,23 @@ echo "======================="
 echo "Download ngrok"
 echo "======================="
 rm -rf ngrok ngrok.zip ngrok.sh > /dev/null 2>&1
-wget -O ng.sh https://raw.githubusercontent.com/violatv/rdp/main/GCngrok.sh > /dev/null 2>&1
-chmod +x ng.sh
-./ng.sh
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok.zip > /dev/null 2>&1
+read -p "Paste Ngrok Authtoken: " CRP
+./ngrok authtoken $CRP
+chmod +x ngrok.sh
+./ngrok.sh
 clear
-./ngrok tcp --region us 3389 &>/dev/null &
+echo "======================="
+echo choose ngrok region
+echo "======================="
+echo "us - United States (Ohio)"
+echo "eu - Europe (Frankfurt)"
+echo "ap - Asia/Pacific (Singapore)"
+echo "au - Australia (Sydney)"
+echo "sa - South America (Sao Paulo)"
+echo "jp - Japan (Tokyo)"
+echo "in - India (Mumbai)"
+read -p "choose ngrok region: " CRP
+./ngrok tcp --region $CRP 3389 &>/dev/null &
 echo "======================="
