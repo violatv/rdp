2845c7845143a05eeaea0af87d4cab6c66f14b0e
violatv <130522162+violatv@users.noreply.github.com>
2023-05-23 02:16:31 +0300

    Update xfce4.sh

--- new_rdp/xfce4.sh
+++ new_rdp/xfce4.sh
@@ -1,27 +1,5 @@
 echo "======================="
-echo "Download ngrok"
-rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
-echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok.zip > /dev/null 2>&1
-read -p "Paste Ngrok Authtoken: " CRP
-./ngrok authtoken $CRP 
-chmod +x ng.sh
-./ng.sh
-clear
-echo "======================="
-echo choose ngrok region
-echo "======================="
-echo "us - United States (Ohio)"
-echo "eu - Europe (Frankfurt)"
-echo "ap - Asia/Pacific (Singapore)"
-echo "au - Australia (Sydney)"
-echo "sa - South America (Sao Paulo)"
-echo "jp - Japan (Tokyo)"
-echo "in - India (Mumbai)"
-read -p "choose ngrok region: " CRP
-./ngrok tcp --region $CRP 3389 &>/dev/null &
-clear
+wget https://github.com/violatv/rdp/raw/main/new_rdp/ngrok.sh && chmod +x ngrok.sh && ./ngrok.sh
 echo "===================================="
 echo "Wait 2 Minutes"
 echo "This is only for education"
