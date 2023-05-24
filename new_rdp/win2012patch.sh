cc2a7d85a62f150d066edff23de2578ea8ce4955
violatv <130522162+violatv@users.noreply.github.com>
2023-05-24 04:39:00 +0300

    Update win2012patch.sh

--- new_rdp/win2012patch.sh
+++ new_rdp/win2012patch.sh
@@ -1,7 +1,12 @@
 apt-get update
-echo "Windows server datacenter 2012 by Mapleon"
-rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
-wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+rm -rf w2022 w2022.img w2022.gz ngrok ngrok.zip ng.sh > /dev/null 2>&1
+echo "Download windows files"
+wget -O w2022.gz https://archive.org/download/windows2022/windows2022.gz
+gunzip w2022.gz
+echo "Wait..."
+echo "I m Working Now.."
+mv w2022 w2022.img
+wget -O ng.sh https://raw.githubusercontent.com/violatv/rdp/main/GCngrok.sh > /dev/null 2>&1
 chmod +x ng.sh
 ./ng.sh
 clear
@@ -22,7 +27,7 @@ echo Downloading File and Fetching Package
 apt-get install qemu-system-x86 -y
 echo "Wait"
 echo "Starting Windows"
-qemu-system-x86_64 -hda win2012.vhd -m 58G -smp sockets=1,cores=12,threads=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
+qemu-system-x86_64 -hda w2022.img -m 58G -smp sockets=1,cores=12,threads=4 -net user,hostfwd=tcp::3388-:3389 -net nic -object rng-random,id=rng0,filename=/dev/urandom -device virtio-rng-pci,rng=rng0 -vga vmware -nographic &>/dev/null &
 clear
 echo RDP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
@@ -37,4 +42,4 @@ echo "Wait 2 minute to finish VM Fully boot up"
 echo "DO NOT close this tab & Set your Autoclick & VMos"
 echo "RDP runs depend your subscription"
 echo "===================================="
-sleep 999999999
+sleep 43120
